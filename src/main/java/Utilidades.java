fc81431b2a3452a26de7c3df811ec25a2fca92a2
Diegocd <32700537+Diegocd@users.noreply.github.com>
2018-02-07 10:56:49 +0100

    Update Utilidades.java

--- src/main/java/Utilidades.java
+++ src/main/java/Utilidades.java
@@ -2,42 +2,45 @@
 public class Utilidades {
 
     public static int[] ordenar(int num1, int num2, int num3) {
-        int[] ordenados = new int[3];
+        /*    int[] ordenados = new int[3];
 
-        if (num1 < num2 && num1 < num3) {
-            ordenados[0] = num1;
-            if (num2 < num3) {
-                ordenados[1] = num2;
-                ordenados[2] = num3;
-            } else {
-                ordenados[1] = num3;
-                ordenados[2] = num2;
-            }
-        } else if (num2 < num1 && num2 < num3) {
-            ordenados[0] = num2;
-            if (num1 < num3) {
-                ordenados[1] = num1;
-                ordenados[2] = num3;
-            } else {
-                ordenados[1] = num3;
-                ordenados[2] = num1;
-            }
-        } else if ( num3 < num2 && num3 < num1) {
-            ordenados[0] = num3;
-            if (num2 < num1) {
-                ordenados[1] = num2;
-                ordenados[2] = num1;
-            } else {
-                ordenados[1] = num1;
-                ordenados[2] = num2;
-            }
-        } else { // Números iguales
-            ordenados = new int[]{num1, num2, num3};
-        }
+         if (num1 < num2 && num1 < num3) {
+         ordenados[0] = num1;
+         if (num2 < num3) {
+         ordenados[1] = num2;
+         ordenados[2] = num3;
+         } else {
+         ordenados[1] = num3;
+         ordenados[2] = num2;
+         }
+         } else if (num2 < num1 && num2 < num3) {
+         ordenados[0] = num2;
+         if (num1 < num3) {
+         ordenados[1] = num1;
+         ordenados[2] = num3;
+         } else {
+         ordenados[1] = num3;
+         ordenados[2] = num1;
+         }
+         } else if ( num3 < num2 && num3 < num1) {
+         ordenados[0] = num3;
+         if (num2 < num1) {
+         ordenados[1] = num2;
+         ordenados[2] = num1;
+         } else {
+         ordenados[1] = num1;
+         ordenados[2] = num2;
+         }
+         } else { // Números iguales
+         ordenados = new int[]{num1, num2, num3};
+         }
 
-        return ordenados;
+         return ordenados;
         
-    /*  SOLUCIÓN
+         SOLUCIÓN
+         */
+        
+        
         int[] ordenados = new int[3];
 
         if (num1 <= num2 && num1 <= num3) {
@@ -67,11 +70,9 @@ public class Utilidades {
                 ordenados[1] = num1;
                 ordenados[2] = num2;
             }
-        } 
-        return ordenados;    
-        
-    */    
-        
+        }
+        return ordenados;
+
     }
 
 }
